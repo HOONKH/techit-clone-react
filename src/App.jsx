766ff3f876103a4891efa67dfa4e0bdf48864988
HOONKH <boospa0305@gmai.com>
2023-11-16 15:30:31 +0900

    layout

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+const App = () => {
+  return (
+    <>
+      <div className="h-[42px] bg-purple-200">Top Banner</div>
+      <header className="bg-red-100 h-16">Header</header>
+      <div className="bg-red-100 h-[400px]">banner</div>
+      <section className="mt-20 bg-gray-100 max-w-screen-xl mx-auto px-6">
+        <div className="bg-red-100 h-[64.5px]">KDT</div>
+        <div className="bg-purple-100 grid grid-cols-4 gap-6 justify-items-start">
+          <div className="bg-blue-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-blue-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-blue-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-blue-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-blue-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-blue-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-blue-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-blue-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-blue-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-blue-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-blue-100 w-[290px] h-[416px]">Card</div>
+        </div>
+      </section>
+      <section className="mt-20 bg-gray-100 max-w-screen-xl mx-auto px-6">
+        <div className="bg-red-100 h-[64.5px]">Schedule</div>
+        <div className="bg-blue-100 h-[280px]">KDT</div>
+      </section>
+      <div className="bg-yellow-100 h-[206px] mt-20">Alarm Banner</div>
+      <section className="mt-20 bg-gray-100 max-w-screen-xl mx-auto px-6">
+        <div className="bg-red-100 h-[64.5px]">Onboarding</div>
+        <div className="bg-purple-100 grid grid-cols-3 gap-6 justify-items-start">
+          <div className="bg-blue-100 w-[397px] h-[316px]">Card</div>
+          <div className="bg-blue-100 w-[397px] h-[316px]">Card</div>
+          <div className="bg-blue-100 w-[397px] h-[316px]">Card</div>
+          <div className="bg-blue-100 w-[397px] h-[316px]">Card</div>
+          <div className="bg-blue-100 w-[397px] h-[316px]">Card</div>
+        </div>
+      </section>
+      <section className="mt-20 bg-gray-100 max-w-screen-xl mx-auto px-6">
+        <div className="bg-red-100 h-[64.5px]">Education</div>
+        <div className="bg-purple-100 grid grid-cols-2 gap-6 justify-items-start">
+          <div className="bg-blue-100 w-[604px] h-[310px]">Card</div>
+          <div className="bg-blue-100 w-[604px] h-[310px]">Card</div>
+          <div className="bg-blue-100 w-[604px] h-[310px]">Card</div>
+        </div>
+      </section>
+      <footer className="mt-40 max-w-screen-xl mx-auto bg-green-200 h-[418px]"></footer>
+    </>
+  );
+};
+
+export default App;
